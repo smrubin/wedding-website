af2ab24d8b3ecbd593e0b6e57a783dad15a2c9e5
smrubin <sam@storyblocks.com>
2018-12-05 23:36:15 -0500

    [Cleanup] Removes social buttons and misc cleanup

--- js/scripts.js
+++ js/scripts.js
@@ -136,29 +136,6 @@ $(document).ready(function () {
 
     });
 
-    /********************** Social Share buttons ***********************/
-    var share_bar = document.getElementsByClassName('share-bar');
-    var po = document.createElement('script');
-    po.type = 'text/javascript';
-    po.async = true;
-    po.src = 'https://apis.google.com/js/platform.js';
-    var s = document.getElementsByTagName('script')[0];
-    s.parentNode.insertBefore(po, s);
-
-    for (var i = 0; i < share_bar.length; i++) {
-        var html = '<iframe allowtransparency="true" frameborder="0" scrolling="no"' +
-            'src="https://platform.twitter.com/widgets/tweet_button.html?url=' + encodeURIComponent(window.location) + '&amp;text=' + encodeURIComponent(document.title) + '&amp;via=ramswarooppatra&amp;hashtags=rachelandsam&amp;count=horizontal"' +
-            'style="width:105px; height:21px;">' +
-            '</iframe>' +
-
-            '<iframe src="//www.facebook.com/plugins/like.php?href=' + encodeURIComponent(window.location) + '&amp;width&amp;layout=button_count&amp;action=like&amp;show_faces=false&amp;share=true&amp;height=21&amp;appId=101094500229731&amp;width=150" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:150px; height:21px;" allowTransparency="true"></iframe>' +
-
-            '<div class="g-plusone" data-size="medium"></div>';
-
-        share_bar[i].innerHTML = html;
-        share_bar[i].style.display = 'inline-block';
-    }
-
     /********************** Embed youtube video *********************/
     $('.player').YTPlayer();
 
@@ -181,23 +158,10 @@ $(document).ready(function () {
             id: ''
         },
         data: {
-            // Event title
             title: "Sam and Rachel's Wedding",
-
-            // Event start date
             start: new Date('Oct 25, 2019 19:00'),
-
-            // Event duration (IN MINUTES)
-            // duration: 120,
-
-            // You can also choose to set an end time
-            // If an end time is set, this will take precedence over duration
             end: new Date('Oct 27, 2017 12:00'),
-
-            // Event Address
             address: '6903 Blenheim Road, Charlottesville, VA 24590',
-
-            // Event Description
             description: "We can't wait to see you on our big day. For any queries or issues visit https://rachelandsamwedding.com."
         }
     });
