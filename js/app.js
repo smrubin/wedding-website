4f1245b0186f5ab21a0d8855265840a84d452bd9
smrubin <sam@storyblocks.com>
2019-01-06 18:13:15 -0500

    [Styling] Various style updates and cleanup

--- js/app.js
+++ js/app.js
@@ -86,9 +86,6 @@ $(document).ready(function () {
                     "border-bottom": "none",
                     "padding": "35px 0"
                 });
-                $('header .member-actions').css({
-                    "top": "26px",
-                });
                 $('header .navicon').css({
                     "top": "34px",
                 });
@@ -97,9 +94,6 @@ $(document).ready(function () {
                 $('header').css({
                     "border-bottom": "solid 1px rgba(255, 255, 255, 0.2)",
                     "padding": "50px 0"
-                });
-                $('header .member-actions').css({
-                    "top": "41px",
                 });
                 $('header .navicon').css({
                     "top": "48px",
